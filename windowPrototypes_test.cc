910c77ae94b47b044ff0fc9b1c6c57d9aee6f3e8
Zaneta Wozniak <wozniak_zaneta@wp.pl>
2021-06-12 17:35:20 +0200

    ADD-TEST: window_prototypes_test

--- windowPrototypes_test.cc
+++ windowPrototypes_test.cc
@@ -8,15 +8,21 @@ TEST(WindowProperties_test, BasicAssertions)
     // const int &getWindowWidth();
     //void setWindowWidth(int);
     //void setWindowHeight(int);
-    int width = 50, height = 50;
+    int width = 1366, height = 768;
     test1->setWindowWidth(width);
     test1->setWindowHeight(height);
     EXPECT_EQ(width, test1->getWindowWidth());
     EXPECT_EQ(height, test1->getWindowHeight());
+    
     width=1280; height=720;
     test1->setWindowWidth(width);
     test1->setWindowHeight(height);
     EXPECT_EQ(width, test1->getWindowWidth());
     EXPECT_EQ(height, test1->getWindowHeight());
-
+    
+    width=1920; height=1080;
+    test1->setWindowWidth(width);
+    test1->setWindowHeight(height);
+    EXPECT_EQ(width, test1->getWindowWidth());
+    EXPECT_EQ(height, test1->getWindowHeight());
 }
